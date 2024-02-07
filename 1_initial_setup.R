6a2b2222b5d7be3820eaae653a419134fca62702
kvsathish <karthiksathish2025@u.northwestern.edu>
2024-02-06 20:04:23 -0600

    data found

---
+++ 1_initial_setup.R
@@ -0,0 +1,15 @@
+## R script for progress memo 1
+
+## packages and datasets
+library(tidyverse)
+library(skimr)
+library(ggplot2)
+library(here)
+
+## reading in data ----
+
+cbb_players <- read_csv("data/CollegeBasketballPlayers2009-2021.csv")
+
+View(cbb_players)
+
+
